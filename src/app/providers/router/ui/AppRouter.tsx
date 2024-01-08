0385956d73ede7d9389fdc0dddc6227bdbcaf086
Ivan Kolpakov <vanjaklp95@gmail.com>
2024-01-08 18:07:09 +0300

    navbar init

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,17 +1,15 @@
-import { AboutPageLazy } from 'pages/AboutPage'
-import { MainPageLazy } from 'pages/MainPage'
 import React, { Suspense } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { routeConfig } from 'shared/config/routeConfig/routeConfig'
 
 export default function AppRouter() {
-  // console.log('lalalla',Object.values(routeConfig).map(route => route.element))
 
   return (
     <Suspense fallback={<div>ЗАГРУЗКА...</div>}>
       <Routes>
         {Object.values(routeConfig).map(({ path, element }) => (
           <Route key={path} path={path} element={element} />
+          
         ))}
       </Routes>
     </Suspense>
