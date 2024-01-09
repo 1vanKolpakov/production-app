efc6459ff254d4c6ddcd95094ce2bccde2eda665
Ivan Kolpakov <vanjaklp95@gmail.com>
2024-01-09 18:58:33 +0300

    not found page

--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -1,15 +1,18 @@
 import { AboutPageLazy } from "pages/AboutPage"
 import { MainPageLazy } from "pages/MainPage"
+import { NotFoundPage } from "pages/NotFoundPage"
 import { RouteProps } from "react-router-dom"
 
 export enum AppRoutes {
   MAIN = 'main',
   ABOUT = 'about',
+  NOT_FOUND = 'not_found'
 }
 
 export const RoutePath: Record<AppRoutes, string> = {
   [AppRoutes.MAIN]: '/',
   [AppRoutes.ABOUT]: '/about',
+  [AppRoutes.NOT_FOUND]: '*',
 }
 
 export const routeConfig :Record<AppRoutes, RouteProps> = {
@@ -21,5 +24,9 @@ export const routeConfig :Record<AppRoutes, RouteProps> = {
   [AppRoutes.ABOUT]: {
     path: RoutePath.about,
     element: <AboutPageLazy />
+  },
+  [AppRoutes.NOT_FOUND]: {
+    path: RoutePath.not_found,
+    element: <NotFoundPage />
   },
 }
